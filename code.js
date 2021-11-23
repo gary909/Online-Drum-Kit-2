5fe2e965993c114da406d994688292db4671e98e
gary <gary.white@lodgit.com>
2021-11-23 17:29:46 +0100

    Audio playing, not multiple though

--- code.js
+++ code.js
@@ -1,11 +1,36 @@
 document.addEventListener('keydown', handleClick)
 
-// const Q = document.getElementById("Q");
-// var Q = data-key="81";
+/*
+KEYCODE INFO
+Q = 81
+W = 87
+E = 69
+R = 82
+T = 84
+Y = 89
+*/
 
 function handleClick(e){
+    //Pre-load otherwise only half sample is played
+    document.getElementById('Q').load();
+    document.getElementById('W').load();
+    document.getElementById('E').load();
+    document.getElementById('R').load();
+    document.getElementById('T').load();
+    document.getElementById('Y').load();
     var keyCode = e.keyCode;
     if (keyCode == 81){
-        alert("You clicked me!")
+        // alert("You clicked me!")
+        document.getElementById('Q').play();
+    } else if (keyCode == 87) {
+        document.getElementById('W').play();
+    } else if (keyCode == 69) {
+        document.getElementById('E').play();
+    } else if (keyCode == 82) {
+        document.getElementById('R').play();
+    } else if (keyCode == 84) {
+        document.getElementById('T').play();
+    } else if (keyCode == 89) {
+        document.getElementById('Y').play();
     }
 }
