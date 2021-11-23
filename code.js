d67554216d3e880d1733e0223281865ccadf6bcc
gary <gary.white@lodgit.com>
2021-11-23 16:47:27 +0100

    works with specific keypress

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
 document.addEventListener('keydown', handleClick)
 
 // const Q = document.getElementById("Q");
+// var Q = data-key="81";
 
-function handleClick(){
-    alert("You clicked me!")
+function handleClick(e){
+    var keyCode = e.keyCode;
+    if (keyCode == 81){
+        alert("You clicked me!")
+    }
 }
